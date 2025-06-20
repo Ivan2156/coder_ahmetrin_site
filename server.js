dfd504f3a1dd6e8ed7498c219e0cbaee585b96ab
Ivan2156 <160156783+Ivan2156@users.noreply.github.com>
2025-06-20 03:24:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('.'));
+// Видаємо статичні файли з поточної директорії
+app.use(express.static(__dirname));
+
+// Головна сторінка
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'site.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
